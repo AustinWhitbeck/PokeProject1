be70c059c139a2172bc0574723b8d35cc1ca385a
Austin Whitbeck <austinwhitbeckart@gmail.com>
2022-05-22 21:54:26 -0400

    work in progress filtering

--- src/components/molecules/SmallPokeProfileCard/SmallPokeProfileCard.js
+++ src/components/molecules/SmallPokeProfileCard/SmallPokeProfileCard.js
@@ -2,30 +2,51 @@ import { Typography, Button, Box, Card } from '@mui/material';
 import PropTypes from 'prop-types';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import AddIcon from '@mui/icons-material/Add';
-import React from 'react'
+import React from 'react';
 import { SpriteContainer } from './SmallPokeProfileCard.styles';
 
 const SmallPokeProfileCard = (props) => {
-    const { pokemon, addFavoritePokemon } = props;
+  const { pokemon, addFavoritePokemon } = props;
 
   return (
-    <Card sx={{border: '2px black', padding: '20px', display: 'flex', flexDirection: 'column', width:'200px', }}>
-        <Typography variant="h4" sx={{fontSize: '20px', textAlign: 'center'}}>{pokemon.name.toUpperCase()}</Typography>
-        <SpriteContainer>
-            <img src={pokemon.sprites.front_default}/>
-            <img src={pokemon.sprites.back_default}/>
-        </SpriteContainer>
-        <Box sx={{display: 'flex'}}>
-            <Button onClick={() => addFavoritePokemon(pokemon)} startIcon={<FavoriteBorderIcon/>} sx={{ height: '50px', fontSize: '10px'}}>Favorite</Button>
-            <Button startIcon={<AddIcon />} sx={{ height: '50px', fontSize: '10px'}}>Add to Team</Button>
-        </Box>
+    <Card
+      sx={{
+        border: '2px black',
+        padding: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        width: '200px',
+      }}
+    >
+      <Typography variant="h4" sx={{ fontSize: '20px', textAlign: 'center' }}>
+        {pokemon.name.toUpperCase()}
+      </Typography>
+      <SpriteContainer>
+        <img src={pokemon.sprites.front_default} alt="Poke front sprite" />
+        <img src={pokemon.sprites.back_default} alt="Poke back sprite" />
+      </SpriteContainer>
+      <Box sx={{ display: 'flex' }}>
+        <Button
+          onClick={() => addFavoritePokemon(pokemon)}
+          startIcon={<FavoriteBorderIcon />}
+          sx={{ height: '50px', fontSize: '10px' }}
+        >
+          Favorite
+        </Button>
+        <Button
+          startIcon={<AddIcon />}
+          sx={{ height: '50px', fontSize: '10px' }}
+        >
+          Add to Team
+        </Button>
+      </Box>
     </Card>
-  )
-}
+  );
+};
 
 SmallPokeProfileCard.propTypes = {
-    pokemon: PropTypes.instanceOf(Object).isRequired,
-    addFavoritePokemon: PropTypes.func.isRequired,
-}
+  pokemon: PropTypes.instanceOf(Object).isRequired,
+  addFavoritePokemon: PropTypes.func.isRequired,
+};
 
-export default SmallPokeProfileCard
+export default SmallPokeProfileCard;
--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -1,69 +1,94 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react';
 import { getAllPokemon, getAPokemon } from '../../../services/PokeApi';
 import SmallPokeProfileCard from '../../molecules/SmallPokeProfileCard/SmallPokeProfileCard';
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, Input } from '@mui/material';
 import Loader from '../../atoms/Loader/Loader';
 
-
 const HomePage = () => {
-    const [pokeInput, setPokeInput ] = useState('');
-    const [myFavoritePokemon, setMyFavoritePokemon] = useState([]);
-    const [chosenPokemon, setChosenPokemon] = useState(false);
+  const [pokeInput, setPokeInput] = useState('');
+  const [myFavoritePokemon, setMyFavoritePokemon] = useState([]);
+  const [chosenPokemon, setChosenPokemon] = useState(false);
 
-    const [chosenPokeLoading, setChosenPokeLoading ] = useState(false);
+  const [chosenPokeLoading, setChosenPokeLoading] = useState(false);
 
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setPokeInput(e.target.value);
+  };
 
-    const handleInputChange = (e) => {
-        e.preventDefault();
-        setPokeInput(e.target.value);
+  const handleGetOnePokemon = async (pokemon) => {
+    setChosenPokeLoading(true);
+    await getAPokemon(pokemon.toLowerCase()).then((result) => {
+      setChosenPokemon(result.data);
+      setChosenPokeLoading(false);
+    });
+  };
+
+  const addFavoritePokemon = (pokemon) => {
+    const newFavs = [...myFavoritePokemon];
+    console.log(
+      'result of filtering',
+      myFavoritePokemon.filter((favPoke) => favPoke.name === pokemon.name)
+    );
+    if (!myFavoritePokemon.includes(pokemon)) {
+      newFavs.push(pokemon);
     }
+    setMyFavoritePokemon(newFavs);
+  };
 
-    const handleGetOnePokemon  =  async (pokemon) => {
-        setChosenPokeLoading(true);
-         await getAPokemon(pokemon.toLowerCase()).then((result) => {
-                setChosenPokemon(result.data);
-                setChosenPokeLoading(false);
-            })
-        
-    }
-
-    const addFavoritePokemon = (pokemon) => {
-        const newFavs = [...myFavoritePokemon];
-        console.log('result of filtering', myFavoritePokemon.filter((favPoke) => favPoke.name === pokemon.name));
-        if (!myFavoritePokemon.includes(pokemon)){
-        newFavs.push(pokemon);
-        }
-        setMyFavoritePokemon(newFavs);
-    }
+  const filterFavesByName = (e) => {
+    e.preventDefault();
+    const newFavesFiltered = myFavoritePokemon.filter((poke) => {
+      return poke.name.includes(e.target.value.toLowerCase());
+    });
+    console.log('myFavesFiltered', newFavesFiltered);
+    setMyFavoritePokemon(newFavesFiltered);
+  };
 
   return (
     <>
-        <Typography>HomePage</Typography>
-        <h2>Pick a Pokemon!</h2>
-        <div>
-            <input value={pokeInput} onChange={(e) => handleInputChange(e)} placeholder="pick a pokemon!"/>
-            <button onClick={() => handleGetOnePokemon(pokeInput)}>Get this pokemon!</button>
-        </div>
-        <div>
-            <button onClick={getAllPokemon}>Get all pokemon</button>
-        </div>
-        <Loader loading={chosenPokeLoading}>
+      <Typography>HomePage</Typography>
+      <h2>Pick a Pokemon!</h2>
+      <div>
+        <input
+          value={pokeInput}
+          onChange={(e) => handleInputChange(e)}
+          placeholder="pick a pokemon!"
+        />
+        <button onClick={() => handleGetOnePokemon(pokeInput)}>
+          Get this pokemon!
+        </button>
+      </div>
+      <div>
+        <button onClick={getAllPokemon}>Get all pokemon</button>
+      </div>
+      <Loader loading={chosenPokeLoading}>
         {chosenPokemon !== false ? (
-            <SmallPokeProfileCard pokemon={chosenPokemon} addFavoritePokemon={addFavoritePokemon}/>
+          <SmallPokeProfileCard
+            pokemon={chosenPokemon}
+            addFavoritePokemon={addFavoritePokemon}
+          />
         ) : (
-            ''
+          ''
         )}
-        </Loader>
-        <Typography variant="h4" component="h2" sx={{marginTop: '10px'}}>My Favorite Pokemon</Typography>
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '10px'}}>
-            {myFavoritePokemon.length ? (
-                myFavoritePokemon.map((pokemon) => <SmallPokeProfileCard pokemon={pokemon} addFavoritePokemon={addFavoritePokemon}/>)
-            ) : (
-                'no favorites'
-            )}
-        </Box>
+      </Loader>
+      <Typography variant="h4" component="h2" sx={{ marginTop: '10px' }}>
+        My Favorite Pokemon
+      </Typography>
+      <Input onChange={(e) => filterFavesByName(e)}></Input>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
+        {myFavoritePokemon.length
+          ? myFavoritePokemon.map((pokemon) => (
+              <SmallPokeProfileCard
+                key={pokemon.name}
+                pokemon={pokemon}
+                addFavoritePokemon={addFavoritePokemon}
+              />
+            ))
+          : 'no favorites'}
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
